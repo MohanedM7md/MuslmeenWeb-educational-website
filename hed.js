4754dc422046eb0ef6c2b147996248a6f8bf3ba4
Mohaned <mohandmohamed194@gmail.com>
2023-11-11 10:02:05 +0200

    deleted hed.js

--- hed.js
+++
@@ -1,4 +0,0 @@
-[...Array.from(document.html.querySelectorAll('*'))][29].style.backgroundColor
-
-let yla = [...Array.from(document.body.querySelectorAll('*'))][29];
-console.log(yla.style.backgroundColor.red )
